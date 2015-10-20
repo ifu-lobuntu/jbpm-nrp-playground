f878ff60b8083b50c43d8e55696ea7e11b3a54c1
kris <>
2015-10-20 14:38:24 +0200

    Created asset Product.java {/networked-economic-collaboration/src/main/java/net/economiccollaboration/library/Product.java}

---
+++ networked-economic-collaboration/src/main/java/net/economiccollaboration/library/Product.java
@@ -0,0 +1,49 @@
+/**Do not edit: generated from uml.*/
+package net.economiccollaboration.library;
+import net.economiccollaboration.library.ProductUnitOfMeasurement;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToOne;
+@Entity(name="Product")
+@Table(name="product")
+public class Product{
+  @Id
+  @GeneratedValue
+  Long id;
+  @Basic()
+  @Column(name="name")
+  private String name = "";
+  @ManyToOne()
+  @JoinColumns(value={
+        @JoinColumn(name="unit_of_measure_id",referencedColumnName="id")}
+  )
+  private ProductUnitOfMeasurement unitOfMeasure = null;
+  public Product(){
+  }
+  public Long getId(){
+    return this.id;
+  }
+  public void setId(Long value){
+    this.id=value;
+  }
+  public String getName(){
+    String result = this.name;
+    return result;
+  }
+  public ProductUnitOfMeasurement getUnitOfMeasure(){
+    ProductUnitOfMeasurement result = this.unitOfMeasure;
+    return result;
+  }
+  public void setName(String newName){
+    this.name=newName;
+  }
+  public void setUnitOfMeasure(ProductUnitOfMeasurement newUnitOfMeasure){
+    this.unitOfMeasure=newUnitOfMeasure;
+  }
+}
